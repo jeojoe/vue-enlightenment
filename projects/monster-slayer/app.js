247f3f92eea26910c50a8fe9e65cd8b3c749719e
Jirat Onaree <jirat.onaree@gmail.com>
2017-06-28 21:46:10 +0700

    :fire: Monster Slayer :fire: : healing flag workaround on calculateLife

--- projects/monster-slayer/app.js
+++ projects/monster-slayer/app.js
@@ -29,18 +29,28 @@ var app = new Vue({
       this.calculateLife(-heroDamages, -monsterDamages);
     },
     heal: function() {
-
+      // Random Hero's healing and Monster's damages
+      const heroHealing = Math.ceil(Math.random() * 10);
+      const monsterDamages = Math.ceil(Math.random() * 10);
+      // Calculate Life
+      this.calculateLife(heroHealing, -monsterDamages, 'healing');
     },
     giveUp: function() {
 
     },
-    calculateLife: function(effectOnMonster, effectOnHero) {
+    calculateLife: function(effectOnMonster, effectOnHero, healing) {
       let hasWinner = false;
       let heroLife_left = this.heroLife;
       let monsterLife_left = this.monsterLife;
 
-      heroLife_left += effectOnHero;
-      monsterLife_left += effectOnMonster;
+      if (healing) {
+        heroLife_left += effectOnHero;
+        // A little fucked up logic : effectOnMonster is healing lol
+        heroLife_left += effectOnMonster;
+      } else {
+        heroLife_left += effectOnHero;
+        monsterLife_left += effectOnMonster;
+      }
       // console.log(heroLife, monsterLife);
       if (heroLife_left <= 0 && monsterLife_left <= 0) {
         this.heroLife = 0;
