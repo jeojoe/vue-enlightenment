88f58c6580a6a89a77aa1fab0aec51751a762587
Jirat Onaree <jirat.onaree@gmail.com>
2017-06-29 11:53:16 +0700

    Instance life cycle

---
+++ instance.js
@@ -0,0 +1,36 @@
+var instanceApp1 = new Vue({
+  el: '#instance-app1',
+  data: {
+    text: 'Hey',
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()');
+  },
+  updated: function() {
+    console.log('updated()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
+  },
+  methods: {
+    destroy: function() {
+      // detach Vue instance from HTML
+      this.$destroy();
+    },
+  },
+})
