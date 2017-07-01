fc1d782122f409cdf0448481af6333719100f743
Jirat Onaree <jirat.onaree@gmail.com>
2017-07-01 09:51:51 +0700

    plugin semantic-ui-css.js

--- projects/vue-quote/src/main.js
+++ projects/vue-quote/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// require('semantic-ui-css/semantic.css');
+require('semantic-ui-css/semantic.js');
+
 new Vue({
   el: '#app',
   render: h => h(App)
